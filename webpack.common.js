2e50c13a74b3b08645e2e214c8584f2645ce7157
Barry Molina <bazzaboy@gmail.com>
2021-08-29 11:04:56 -0500

    clean up common.js config

--- webpack.common.js
+++ webpack.common.js
@@ -4,54 +4,52 @@ const Dotenv = require('dotenv-webpack');
 
 
 module.exports = {
-  //Our index file
-    entry: ['./src/global.css', './src/index.js'],
-  //Where we put the production code
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-        // publicPath: '/',
-    },
+	entry: ['./src/global.css', './src/index.js'],
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js',
+		// publicPath: '/',
+	},
     module: {
-        rules: [
-      //Allows use of modern javascript
-            {
-                test: /\.js?$/,
-                exclude: /node_modules/, //don't test node_modules folder
-                use: {
-                    loader: 'babel-loader',
-                },
-            },
-      //Allows use of svelte
-            {
-                test: /\.svelte$/,
-                use: {
-                    loader: 'svelte-loader',
-                },
-            },
-      //Allows use of CSS
-            {
-                test: /\.css$/,
-                use: [
-									'css-loader'
-								],
-            },
-      //Allows use of images
-            {
-                test: /\.(jpg|jpeg|png|svg)$/,
-                use: 'file-loader',
-            },
-        ],
+			rules: [
+				// Allow use of modern javascript
+				{
+					test: /\.js?$/,
+					exclude: /node_modules/, //don't test node_modules folder
+					use: {
+						loader: 'babel-loader',
+					},
+				},
+				// Allow use of svelte
+				{
+					test: /\.svelte$/,
+					use: {
+						loader: 'svelte-loader',
+					},
+				},
+				// Allow use of CSS
+				{
+					test: /\.css$/,
+					use: [
+						'css-loader'
+					],
+				},
+				// Allow use of images
+				{
+					test: /\.(jpg|jpeg|png|svg)$/,
+					use: 'file-loader',
+				},
+			],
     },
-		//this is what enables users to leave off the extension when importing
+		// Enable users to leave off the extension when importing
     resolve: {
 			extensions: ['.mjs', '.js', '.svelte'],
     },
     plugins: [
-			//This gets all our css and put in a unique file
+			// Get all css and put in a unique file
 			new MiniCssExtractPlugin(),
-			//take our environment variable in .env file
-			//And it does a text replace in the resulting bundle for any instances of process.env.
+			// Take environment variable in .env file
+			// And do a text replace in the resulting bundle for any instances of process.env.
 			new Dotenv(),
     ],
 };
